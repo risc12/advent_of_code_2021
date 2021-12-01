6c76b98675a2fb24afe24d6413cda394bcccfb18
Remco Peggeman <r.peggeman@youngcapital.nl>
2021-12-01 17:14:35 +0100

    Simplify solution

--- day_one/solve.js
+++ day_one/solve.js
@@ -1,49 +1,44 @@
 const fs = require('fs');
 
-const inputArray = fs.readFileSync('input.txt').toString().split("\n").map(el => parseInt(el));
+const fileToRead = process.argv[2] || 'input.txt';
 
-function findIncreasedElements(array) {
-  return array.map((element, index, array) => {
+const inputArray = fs.readFileSync(fileToRead).toString().split("\n").map(el => parseInt(el));
+
+function getAmountOfIncreasedElements(array) {
+  let amountOfIncreases = 0;
+
+  array.forEach((element, index, array) => {
     if(index === 0)  return false;
 
     const prev = array[index - 1];
 
-    return element > prev;
+    if(element > prev) amountOfIncreases++;
   })
 
+  return amountOfIncreases;
 }
 
 // https://adventofcode.com/2021/day/1
 function partOne() {
-  const increasedElements = findIncreasedElements(inputArray);
-  const amountOfIncreasedElements = increasedElements.filter(el => el).length;
-
-  return amountOfIncreasedElements
+  return getAmountOfIncreasedElements(inputArray);
 }
 
 // https://adventofcode.com/2021/day/1#part2
 function partTwo() {
-  const slidingWindows = inputArray
+  const sumOfSlidingWindows = inputArray
     .map((element, index, array) => {
       const nextElement = array[index + 1];
       const elementAfterThat = array[index + 2];
+
       const isSlidingWindowPossible = nextElement && elementAfterThat;
 
       if(!isSlidingWindowPossible) return undefined;
 
-      return [element, nextElement, elementAfterThat];
+      return element + nextElement + elementAfterThat;
     })
-    .filter(el => el);
 
-  const sumOfSlidingWindows = slidingWindows.map(element => {
-    return element.reduce((acc, curr) => acc + curr);
-  });
-
-  const increasedElements = findIncreasedElements(sumOfSlidingWindows);
-
-  const amountOfIncreasedElements = increasedElements.filter(el => el).length;
-
-  return amountOfIncreasedElements;
+  return getAmountOfIncreasedElements(sumOfSlidingWindows);
 }
 
-console.log(partTwo())
+console.log(partOne());
+console.log(partTwo());
